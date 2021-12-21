b10f9c3fbc79cc8967500a7225b0e99c65d5c488
karinaramos0401 <84257110+karinaramos0401@users.noreply.github.com>
2021-12-21 12:15:32 -0300

    add icon image

--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -1,14 +1,15 @@
 import React from 'react'
 
 import Lupa from '../../images/lupa.png'
+// import Icon from '../../images/2-2-anime-png-pic.png'
 
 import './Search.scss'
 
 const Search = ({type = 'search', placeholder}) => (
-    <div className='kkk'>
+  <div className='kkk'>
+     {/* <img className="icon" src={Icon} alt="icon" /> */}
       <h1 className="welcome">, fulano</h1>
-      <p className='subtitle'>Let's watch animes together</p>
-      {/* Ao lado do título ficar a fotinho do login da pessoa */}
+      <p className='subtitle'>Let's watch together</p>
       <img className="lupa" src={Lupa} alt="lupa" />
       <input className='pesquisa' type={type}
       placeholder={placeholder} />     
