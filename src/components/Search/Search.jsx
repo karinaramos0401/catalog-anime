ec7045acae7a1bb81966d085d1e8bf8956dac8b9
karinaramos0401 <84257110+karinaramos0401@users.noreply.github.com>
2021-12-21 11:32:58 -0300

    input search style

--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 
-import Lupa from '../../images/loupe.png'
+import Lupa from '../../images/lupa.png'
 
 import './Search.scss'
 
 const Search = ({type = 'search', placeholder}) => (
-    <div>
-      <h1>hello, fulano</h1>
-      <p>Let's watch animes together</p>
+    <div className='kkk'>
+      <h1 className="welcome">, fulano</h1>
+      <p className='subtitle'>Let's watch animes together</p>
+      {/* Ao lado do título ficar a fotinho do login da pessoa */}
       <img className="lupa" src={Lupa} alt="lupa" />
       <input className='pesquisa' type={type}
       placeholder={placeholder} />     
