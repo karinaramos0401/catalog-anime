d9596749cffddfbef73d99cd4638abdd5e0b6cdb
karinaramos0401 <84257110+karinaramos0401@users.noreply.github.com>
2022-01-05 08:47:37 -0300

    update search branch

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,12 +2,15 @@ import React from 'react'
 
 import Search from '../../components/Search/Search'
 
-const Home = () => (
-  <Search
-  type='search' placeholder='Search'
-  />
-  
 
+
+const Home = () => (
+  <>
+    <Search
+    type="search" placeholder="Search"
+    />
+    <Categories/>
+  </>
 )
 
 
